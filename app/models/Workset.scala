d070816abdbf08debaa29186e46ba9beae9dc237
Nusstorte <david.pinezich@gmail.com>
2015-09-26 16:24:47 +0200

    Updated: Cleanup of the workset model

--- app/models/Workset.scala
+++ app/models/Workset.scala
@@ -1,22 +1,17 @@
 package models
 import play.api.db._
 import play.api.Play.current
-import anorm._
-import anorm.SqlParser._
 import scala.language.postfixOps
-import play.api.Logger
 
 import anorm._
 import anorm.SqlParser._
 
-import io.really.jwt._
-import play.api.libs.json.Json
 /**
- * The Workset
+ * The Workset which is a set of coordinates to be shown now
  *
- * @param id Workset id
- * @param coordinates_id Workset coordinates_id
- * @param set_id Workset set_id
+ * @param id Workset The id of the workset
+ * @param coordinates_id Workset The id of the respected coordinates
+ * @param set_id Workset The set_id which tells the "name" id of the workset
  */
 case class Workset(id: Int, coordinates_id: Int, set_id: Int)
 
@@ -25,22 +20,31 @@ case class Workset(id: Int, coordinates_id: Int, set_id: Int)
  */
 object Workset {
 
+  /**
+   * Find a workset by their set id
+   *
+   * @param set_id The set id of the worksetz
+   * @return List[Workset] The List of worksets found by id
+   */
   def findById(set_id: Int): List[Workset] = {
     DB.withConnection { implicit connection =>
       SQL("SELECT id, coordinates_id, set_id FROM coordinatesToSet WHERE set_id = {set_id}").on('set_id -> set_id).as(Workset.simpleWorkset *)
     }
   }
 
-  def getAllSets(): List[(Int, String)] =  {
+  /**
+   * Returns all worksets
+   *
+   * @return List[(int, String)] All sets in the database
+   */
+  def getAllSets(): List[(Int, String)] = {
     DB.withConnection { implicit connection =>
       val selectSets = SQL("SELECT id, set_name FROM set")
       val sets = selectSets().map(row =>
-        row[Int]("id") -> row[String]("set_name")
-      ).toList
+        row[Int]("id") -> row[String]("set_name")).toList
       sets
     }
   }
-
 
   /**
    * Struct of Workset
@@ -49,8 +53,8 @@ object Workset {
     get[Int]("id") ~
       get[Int]("coordinates_id") ~
       get[Int]("set_id") map {
-      case id~coordinates_id~set_id => Workset(id, coordinates_id, set_id)
-    }
+        case id ~ coordinates_id ~ set_id => Workset(id, coordinates_id, set_id)
+      }
   }
 
 }
